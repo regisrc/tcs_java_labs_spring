583f903ab8812116c2abf607cc519bb59454b70d
Filipe Eduardo Regis <Filipe Eduardo Regis@192.168.15.10>
2019-12-09 20:34:56 -0300

    Commit Configuração inicial

---
+++ labssenac/src/main/java/br/com/labssenac/LabssenacApplication.java
@@ -0,0 +1,13 @@
+package br.com.labssenac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabssenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabssenacApplication.class, args);
+	}
+
+}
---
+++ labssenac/src/main/java/br/com/labssenac/models/Professor.java
@@ -0,0 +1,49 @@
+package br.com.labssenac.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String nome;
+	private boolean ativo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
